b081f0e20321e1341364072b8f278718350e8200
Ngoc Tu <tdngoctu@gmail.com>
2021-07-14 16:43:08 +0700

    add func authentication

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -1,8 +1,10 @@
 package oauth
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -40,23 +42,91 @@ func IsPublic(request *http.Request) bool {
 	return request.Header.Get(headerXPublic) == "true"
 }
 
+func GetCallerId(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+	callerId, err := strconv.ParseInt(
+		request.Header.Get(headerXCallerId),
+		10,
+		64,
+	)
+
+	if err != nil {
+		return 0
+	}
+
+	return callerId
+}
+
+func GetCleintId(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+	clientId, err := strconv.ParseInt(
+		request.Header.Get(headerXClientId),
+		10,
+		64,
+	)
+
+	if err != nil {
+		return 0
+	}
+
+	return clientId
+}
+
 func AuthenticateRequest(request *http.Request) *errors.RestErr {
 	if request == nil {
 		return nil
 	}
-
-	accessToken := strings.TrimSpace(
+	cleanRequest(request)
+	accessTokenId := strings.TrimSpace(
 		request.URL.Query().Get(paramAccessToken),
 	)
 
-	if accessToken == "" {
+	if accessTokenId == "" {
 		return nil
 	}
+
+	at, err := getAccessToken(accessTokenId)
+	if err != nil {
+		return err
+	}
+	request.Header.Add(headerXCallerId, fmt.Sprintf("%v", at.ClientId))
+	request.Header.Add(headerXCallerId, fmt.Sprintf("%v", at.UserId))
 	return nil
 }
 
-func getAccessToken(accessToken string) (*accessToken, *errors.RestErr) {
+func cleanRequest(request *http.Request) {
+	if request == nil {
+		return
+	}
+	request.Header.Del(headerXClientId)
+	request.Header.Del(headerXCallerId)
+}
+
+func getAccessToken(accessTokenId string) (*accessToken, *errors.RestErr) {
 	response := oauthRestClient.Get(
-		fmt.Sprintf("/oauth/access_token/%s", accessToken),
+		fmt.Sprintf("/oauth/access_token/%s", accessTokenId),
 	)
+	if response == nil || response.Response == nil {
+		return nil, errors.NewInternalServerErr("Invalid access Token")
+	}
+
+	if response.StatusCode > 299 {
+		var restErr errors.RestErr
+		if err := json.Unmarshal(response.Bytes(), &restErr); err != nil {
+			return nil, errors.NewInternalServerErr("Invalid body Request")
+		}
+		return nil, &restErr
+	}
+
+	var at accessToken
+
+	if err := json.Unmarshal(response.Bytes(), &at); err != nil {
+		return nil, errors.NewInternalServerErr("Error while Unmashal")
+	}
+
+	return &at, nil
 }
